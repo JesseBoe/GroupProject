dda0d44747a5abd167894b1403b5451a73603b00
Craig Simmons <craig.a.simmons@gmail.com>
2019-04-11 21:09:11 -0700

    Added Firebase initialization to logic.js. Also added an images directory

---
+++ assets/javascript/logic.js
@@ -0,0 +1,11 @@
+
+  var config = {
+    apiKey: "AIzaSyBSb8EQFl1qPtsOwtgdyw4UpI17DV77nZs",
+    authDomain: "snackify-3ccd8.firebaseapp.com",
+    databaseURL: "https://snackify-3ccd8.firebaseio.com",
+    projectId: "snackify-3ccd8",
+    storageBucket: "snackify-3ccd8.appspot.com",
+    messagingSenderId: "51969939809"
+  };
+
+  firebase.initializeApp(config);
