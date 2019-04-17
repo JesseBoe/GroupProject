3a235bb9306b8f9a22f29530f290fafbc78bd21c
Craig Simmons <craig.a.simmons@gmail.com>
2019-04-17 09:57:43 -0700

    minor syntax corrections. Crude mockup of modal

--- assets/javascript/auth.js
+++ assets/javascript/auth.js
@@ -1,4 +1,5 @@
   $(document).ready(function() {
+    
       // Initialize Firebase
       const config = {
           apiKey: "AIzaSyDw-EkMjzTgjFrrEgj--Uz6Iw4H078bBk0",
@@ -59,21 +60,6 @@
               // this will occur on initial load or refresh as no one is logged in
               console.log("Logout if usr=F - LogoutBtn disabled");
           }
-      });
-
-      firebase.auth().signInWithPopup(providerGoogle).then(function(result) {
-          // This gives you a Google Access Token. You can use it to access the Google API.
-          var token = result.credential.accessToken;
-          // The signed-in user info.
-          var user = result.user;
-      }).catch(function(error) {
-          // Handle Errors here.
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          // The email of the user's account used.
-          var email = error.email;
-          // The firebase.auth.AuthCredential type that was used.
-          var credential = error.credential;
       });
 
       // Create a new user - hook to a register button
