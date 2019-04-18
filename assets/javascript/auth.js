536397a3c125a56557a7ec6e44c4031e32d98f75
Craig Simmons <craig.a.simmons@gmail.com>
2019-04-17 21:31:41 -0700

    In theory, you shouldn't be able to evade logging in or registering. One bug exists when you hit register Btn 2x with no credentials - you can access the app. Otherwise,  the modal keep you in a loop until you are successful

--- assets/javascript/auth.js
+++ assets/javascript/auth.js
@@ -12,13 +12,6 @@
       var currentUser = firebase.auth().currentUser;
       var auth = firebase.auth();
 
-      //Initial setting of disabled attribute on Buttons - where false = visible/not disabled
-      $("#registerBtn").attr("disabled", false);
-      $("#loginBtn").attr("disabled", false);
-      $("#auth-LogoutBtn").attr("disabled", true);
-
-      //Event Listeners 
-
       // Register button - click will create new user 
       $("#registerBtn").on("click", function(event) {
           event.preventDefault();
@@ -31,45 +24,24 @@
           event.preventDefault();
           email = $("#email-input").val();
           password = $("#pass-input").val();
-          loginUser(email, password);
-      });
-
-      // Logout  button -  click will logout  user
-      $("#auth-LogoutBtn").on("click", function(event) {
-          logoutUser();
-      });
-
-      // Listener for database auth state changes - just changes button state 
-      //(active vs disabled) depending on whether user is logged in or not.
-      firebase.auth().onAuthStateChanged(function(user) {
-          console.log('onAuthStateChanged::USER', user);
-          window.user = user;
-          // user is undefined if no user signed in.
-          if (user) {
-              console.log(user);
-              currentUser = user;
-              $("#auth-LogoutBtn").attr("disabled", false);
-          } 
-          else {
-              // this will occur on initial load or refresh as no one is logged in
-              $("#auth-LogoutBtn").attr("disabled", true); 
-          }
+         loginUser(email, password);
       });
 
       // Create a new user - hooks to register button
       function createUser(email, password) {
-          alert("Register New User");
+        console.log("local?", firebase.auth.Auth.Persistence.LOCAL);
           if (!email || !password) {
-                alert("Please enter an e-mail and a password")
+                $('#auth-nopwduid').modal();
           } 
           else {    
                 firebase.auth().createUserWithEmailAndPassword(email, password)
                 .then(function(res) {
-                      alert("Registration successful")
+                    console.log("Registration successful")
+                    modal.style.display = "none";
                 })
                 .catch(function(error) {
                     console.log(error.message);
-                    alert(error.message);
+                    $('#auth-nosuchuser').modal();
                 });
           }
       }
@@ -77,27 +49,20 @@
       // User login - hooks to login button
       function loginUser(email, password) {
           console.log("local?", firebase.auth.Auth.Persistence.LOCAL);
-          alert("Login User");
           if (!email || !password) {
-              alert("Please enter an e-mail and a password") // change to modal
-          } else {
+              $('#auth-nopwduid').modal();
+          } 
+          else {
               firebase.auth().signInWithEmailAndPassword(email, password)
               .then(function(res) {
-                    alert("Login successful")
+                  modal.style.display = "none";
               })
               .catch(function(error) {
                   console.log(error.message);
                   alert(error.message);
+                  $('#auth-badpwduid').modal();
               });
           }
-      }
-
-      // Sign out user
-      function logoutUser() {
-        alert("Log User Out");
-          firebase.auth().signOut().catch(function(err) {
-              console.log("logged out")
-          });
       }
 
 // javascript for modal dialog login window below
@@ -110,10 +75,4 @@
           $('#authModalWindow').modal();
       });
 
-      // When the user clicks on <span> (x), close the modal
-      // span.onclick = function() {
-      //    modal.style.display = "none";
-      // }
-
-      
-  });
+    });
